f2fdc709334be164252b5c4f6b584da5be393a36
kdimmick001 <kdimmick001@student.butte.edu>
2017-08-30 01:19:15 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    cout << "_______________________________" << endl;
+    cout << "|  OFF+ +ON                    |" << endl;
+    cout << "|  .------------------------.  |" << endl;
+    cout << "|  |  .-------------------. |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |  |                   | |  |" << endl;
+    cout << "|  |   -------------------  |  |" << endl;
+    cout << "|  |__GAME  BOY___________ /   |" << endl;
+    cout << "|            __________        |" << endl;
+    cout << "|    .       (NINTENDO)        |" << endl;
+    cout << "|  _| |_      ''''''''   .-.   |" << endl;
+    cout << "|-[_   _]-          .-. (   )  |" << endl;
+    cout << "|   |_|            (   ) '-'   |" << endl;
+    cout << "|    '              '-'   A    |" << endl;
+    cout << "|                    B         |" << endl;
+    cout << "|           ____   ____        |" << endl;
+    cout << "|          (____) (____)   ,., |" << endl;
+    cout << "|          Select Start   ;:;: |" <<endl;
+    cout << "|                        ,;:;' /" << endl;
+    cout << "|                       ,:;:'.'" << endl;
+    cout << "|____________________________`" << endl;
+}
+    
---
+++ lab12/lec12.cpp
@@ -0,0 +1,18 @@
+//Created By: April Browne Header
+//Created On: 8/22/2016 Header
+ 
+ #include <iostream> //preprocessor directives sets up input output stream of info
+ using namespace std; //preprocessor directives sets up the standard commands
+ 
+ int main() //main function outputs words
+ {
+     cout<<"Hello "<<"World!"<<endl;
+     cout<<"Hello "<<"Kelsey!"<<endl;
+ }
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!
