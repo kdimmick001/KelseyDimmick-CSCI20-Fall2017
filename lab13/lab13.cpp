94a293812beabe4e562810cefe1f10a579fae8d7
kdimmick001 <kdimmick001@student.butte.edu>
2017-09-01 01:26:27 +0000

    Lab1.3 submission

---
+++ lab13/lab13.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{   // Calculations done for per year
+
+    // double birth = ((60 * 60 * 24 * 365) / 8);                                            
+    // double death = -((60 * 60 * 24 * 365) / 12);               
+    // double international_migrant = ((* 60 * 60 * 24 * 365) / 33);
+    
+    int person_net_gain = ((60 * 60 * 24 * 365) / 12);            
+    int current_year = 2017;
+    int current_population = 325365189;
+    int future_year = 0;
+    
+    cout << "Enter a future year: ";
+    cin >> future_year;
+    future_year = future_year - current_year;
+    int future_population = ((future_year * person_net_gain) + current_population);
+    cout << endl;
+    cout << "The population in " << future_year << " years" << " will be: " << future_population << endl;
+    
+    return 0;
+
+}
