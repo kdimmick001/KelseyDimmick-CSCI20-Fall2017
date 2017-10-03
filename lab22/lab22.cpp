c48ae8e409d6c931b895d55f2d3f358439ec1c93
kdimmick001 <kdimmick001@student.butte.edu>
2017-10-03 21:12:14 +0000

    Lab2.2 submission

--- lab22/lab22.cpp
+++ lab22/lab22.cpp
@@ -9,13 +9,27 @@ using namespace std;
     A program to generate a random number between 1 and 100.
 */
 
-int main () {
-    //Declaring the variable to store the random number as well as the randome number seed.
+// Declaring a random number function to find the random number between 1 and 100.
+int RandomNumber (int num){
+    
+    //Setting the random number seed.
     srand(time(0));
-    int num_between; 
     
     //To generate a random number between 1 and 100.
-    num_between = ((rand() % 100) + 1);
+    num = ((rand() % 100) + 1);
+    
+    //Returning the computed value of the variable num.
+    return num;
+};
+
+//Main function.
+int main () {
+    
+    //Declaring the variable to store the random number.
+    int num_between; 
+    
+    //Calling the random number function with the declared variable in the main.
+    num_between = RandomNumber(num_between);
     
     //Output of the randomly generated number between 1 and 100.
     cout << "Your random number is " << num_between << endl;
@@ -26,18 +40,18 @@ int main () {
 /*
 
 TEST 1:
-Your random number is 80
+Your random number is 43
 
 TEST 2:
-Your random number is 36
+Your random number is 23
 
 TEST3:
-Your random number is 31
+Your random number is 27
 
 TEST 4: 
-Your random number is 2
+Your random number is 61
 
 TEST 5:
-Your random number is 95
+Your random number is 87
 
 */
